ffb0bc3bb1a4774eda461d50bd38c664df5a0e62
Nathan Soares <nathansoares809@gmail.com>
2023-08-18 13:57:19 -0300

    add customer create controller

---
+++ backend/main/src/main/java/br/com/nathan/ecommerce/main/modules/customer/controller/CreateController.java
@@ -0,0 +1,36 @@
+package br.com.nathan.ecommerce.main.modules.customer.controller;
+
+import br.com.nathan.ecommerce.main.core.annotation.BaseController;
+import br.com.nathan.ecommerce.main.core.exceptions.EntityNotFoundException;
+import br.com.nathan.ecommerce.main.core.infra.http.HttpHelper;
+import br.com.nathan.ecommerce.main.core.mapper.Mapper;
+import br.com.nathan.ecommerce.main.core.validator.TypeUtils;
+import br.com.nathan.ecommerce.main.modules.customer.controller.dto.CustomerDTO;
+import br.com.nathan.ecommerce.main.modules.customer.controller.presentationModel.CustomerPM;
+import br.com.nathan.ecommerce.main.modules.customer.domain.Customer;
+import br.com.nathan.ecommerce.main.modules.customer.useCases.CreateUseCase;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Optional;
+
+@BaseController("/customer")
+@AllArgsConstructor
+public class CreateController {
+
+    private final CreateUseCase useCase;
+    private final Mapper<CustomerDTO, Customer> dtoMapper;
+    private final Mapper<Customer, CustomerPM> pmMapper;
+
+    @PostMapping
+    public ResponseEntity<?> handle(@RequestBody CustomerDTO dto) {
+        try {
+            var data = useCase.execute(dtoMapper.map(dto));
+            return HttpHelper.ok(Optional.of(pmMapper.map(data)));
+        } catch (Exception exception) {
+            return HttpHelper.serverError(exception);
+        }
+    }
+}

3f16ab523c7cbc347c0ffce7da7af01ba1fcc8f9
Nathan Soares <nathansoares809@gmail.com>
2023-08-18 13:54:49 -0300

    add customer update use case and controller

---
+++ backend/main/src/main/java/br/com/nathan/ecommerce/main/modules/customer/useCases/UpdateUseCase.java
@@ -0,0 +1,22 @@
+package br.com.nathan.ecommerce.main.modules.customer.useCases;
+
+import br.com.nathan.ecommerce.main.core.mapper.Mapper;
+import br.com.nathan.ecommerce.main.modules.customer.domain.Customer;
+import br.com.nathan.ecommerce.main.modules.customer.repository.CustomerEntity;
+import br.com.nathan.ecommerce.main.modules.customer.repository.CustomerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UpdateUseCase {
+
+    private final CustomerRepository repository;
+    private final Mapper<Customer, CustomerEntity> mapper;
+
+    public Customer execute(Customer customer, Long id) {
+        customer.setId(id);
+        repository.save(mapper.map(customer));
+        return customer;
+    }
+}

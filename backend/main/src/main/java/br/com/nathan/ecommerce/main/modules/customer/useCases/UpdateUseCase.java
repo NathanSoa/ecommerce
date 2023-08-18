745eb543c232be647bf1c4112d5a10e2fc1097c9
Nathan Soares <nathansoares809@gmail.com>
2023-08-18 17:11:44 -0300

    feat: add ID verification to ensure user exists

--- backend/main/src/main/java/br/com/nathan/ecommerce/main/modules/customer/useCases/UpdateUseCase.java
+++ backend/main/src/main/java/br/com/nathan/ecommerce/main/modules/customer/useCases/UpdateUseCase.java
@@ -1,5 +1,6 @@
 package br.com.nathan.ecommerce.main.modules.customer.useCases;
 
+import br.com.nathan.ecommerce.main.core.exceptions.EntityNotFoundException;
 import br.com.nathan.ecommerce.main.core.mapper.Mapper;
 import br.com.nathan.ecommerce.main.modules.customer.domain.Customer;
 import br.com.nathan.ecommerce.main.modules.customer.repository.CustomerEntity;
@@ -15,6 +16,7 @@ public class UpdateUseCase {
     private final Mapper<Customer, CustomerEntity> mapper;
 
     public Customer execute(Customer customer, Long id) {
+        repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Cannot find customer with id: " + id));
         customer.setId(id);
         repository.save(mapper.map(customer));
         return customer;

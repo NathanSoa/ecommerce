665ea95e814272a8ca5a56d22282e949195f0295
Nathan Soares <nathansoares809@gmail.com>
2023-08-18 13:42:47 -0300

    update base entity

--- backend/main/src/main/java/br/com/nathan/ecommerce/main/core/domain/BaseEntity.java
+++ backend/main/src/main/java/br/com/nathan/ecommerce/main/core/domain/BaseEntity.java
@@ -4,11 +4,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.time.OffsetDateTime;
 
 @MappedSuperclass
 @Getter
@@ -19,4 +22,10 @@ public abstract class BaseEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
+
+    @NotNull
+    private Boolean active;
+
+    @PastOrPresent
+    private final OffsetDateTime createdAt = OffsetDateTime.now();
 }

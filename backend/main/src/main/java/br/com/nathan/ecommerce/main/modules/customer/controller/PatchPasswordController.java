9b604693442a6cabc3a756b3efce9ddc776497d5
Nathan Soares <nathansoares809@gmail.com>
2023-08-18 17:11:20 -0300

    feat: add patch password use case

---
+++ backend/main/src/main/java/br/com/nathan/ecommerce/main/modules/customer/controller/PatchPasswordController.java
@@ -0,0 +1,34 @@
+package br.com.nathan.ecommerce.main.modules.customer.controller;
+
+import br.com.nathan.ecommerce.main.core.annotation.BaseController;
+import br.com.nathan.ecommerce.main.core.exceptions.EntityNotFoundException;
+import br.com.nathan.ecommerce.main.core.infra.http.HttpHelper;
+import br.com.nathan.ecommerce.main.core.validator.TypeUtils;
+import br.com.nathan.ecommerce.main.modules.customer.controller.dto.PatchPasswordRequest;
+import br.com.nathan.ecommerce.main.modules.customer.useCases.PatchPasswordUseCase;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@BaseController("/customers")
+@AllArgsConstructor
+public class PatchPasswordController {
+
+    private final PatchPasswordUseCase useCase;
+
+    @PatchMapping("/{id}/password")
+    public ResponseEntity<?> handle(@RequestBody PatchPasswordRequest request, @PathVariable Long id) {
+        try {
+            useCase.execute(id, request.password());
+            return HttpHelper.noContent();
+        } catch (Exception exception) {
+            if(TypeUtils.is(exception, EntityNotFoundException.class)) {
+                return HttpHelper.badRequest(exception);
+            }
+            return HttpHelper.serverError(exception);
+        }
+
+    }
+}
---
+++ backend/main/src/main/java/br/com/nathan/ecommerce/main/modules/customer/useCases/PatchPasswordUseCase.java
@@ -0,0 +1,21 @@
+package br.com.nathan.ecommerce.main.modules.customer.useCases;
+
+import br.com.nathan.ecommerce.main.core.exceptions.EntityNotFoundException;
+import br.com.nathan.ecommerce.main.modules.customer.factory.Validators;
+import br.com.nathan.ecommerce.main.modules.customer.repository.CustomerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class PatchPasswordUseCase {
+
+    private final CustomerRepository repository;
+
+    public void execute(Long id, String password) {
+        var customer = repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Cannot find customer with id: " + id));
+        Validators.passwordValidator().validate(password);
+        customer.setPassword(password);
+        repository.save(customer);
+    }
+}

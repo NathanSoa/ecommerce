87b9a74b8ed6bcbe4c4b4d06c2dd0f4433322e7e
Nathan Soares <nathansoares809@gmail.com>
2023-08-18 13:54:07 -0300

    create customer dto

---
+++ backend/main/src/main/java/br/com/nathan/ecommerce/main/modules/customer/adapter/mapper/CustomerDTOToCustomer.java
@@ -0,0 +1,19 @@
+package br.com.nathan.ecommerce.main.modules.customer.adapter.mapper;
+
+import br.com.nathan.ecommerce.main.core.mapper.Mapper;
+import br.com.nathan.ecommerce.main.modules.customer.controller.dto.CustomerDTO;
+import br.com.nathan.ecommerce.main.modules.customer.domain.Customer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerDTOToCustomer implements Mapper<CustomerDTO, Customer> {
+
+    @Override
+    public Customer map(CustomerDTO raw) {
+        return Customer.Create()
+                .withName(raw.name())
+                .withEmail(raw.email())
+                .withPassword(raw.password())
+                .withCPF(raw.cpf());
+    }
+}
---
+++ backend/main/src/main/java/br/com/nathan/ecommerce/main/modules/customer/controller/dto/CustomerDTO.java
@@ -0,0 +1,4 @@
+package br.com.nathan.ecommerce.main.modules.customer.controller.dto;
+
+public record CustomerDTO(String name, String email, String password, String cpf) {
+}

1403f401b4328bcc29ecd3721222c7b8af1f2142
Nathan Soares <nathansoares809@gmail.com>
2023-08-18 17:11:05 -0300

    fix: fixed http method

--- backend/main/src/main/java/br/com/nathan/ecommerce/main/modules/customer/controller/ActivateController.java
+++ backend/main/src/main/java/br/com/nathan/ecommerce/main/modules/customer/controller/ActivateController.java
@@ -3,12 +3,15 @@ package br.com.nathan.ecommerce.main.modules.customer.controller;
 import br.com.nathan.ecommerce.main.core.annotation.BaseController;
 import br.com.nathan.ecommerce.main.core.exceptions.EntityNotFoundException;
 import br.com.nathan.ecommerce.main.core.infra.http.HttpHelper;
+import br.com.nathan.ecommerce.main.core.mapper.Mapper;
 import br.com.nathan.ecommerce.main.core.validator.TypeUtils;
+import br.com.nathan.ecommerce.main.modules.customer.controller.presentationModel.CustomerPM;
+import br.com.nathan.ecommerce.main.modules.customer.domain.Customer;
 import br.com.nathan.ecommerce.main.modules.customer.useCases.PatchActiveUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Optional;
 
@@ -17,12 +20,13 @@ import java.util.Optional;
 public class ActivateController {
 
     private final PatchActiveUseCase useCase;
+    private final Mapper<Customer, CustomerPM> mapper;
 
-    @PutMapping("/{id}/activate")
+    @PatchMapping("/{id}/activate")
     public ResponseEntity<?> handle(@PathVariable Long id) {
         try {
             var data = useCase.execute(id, true);
-            return HttpHelper.ok(Optional.of(data));
+            return HttpHelper.ok(Optional.of(mapper.map(data)));
         } catch (Exception exception) {
             if(TypeUtils.is(exception, EntityNotFoundException.class)) {
                 return HttpHelper.badRequest(exception);
--- backend/main/src/main/java/br/com/nathan/ecommerce/main/modules/customer/controller/DeactivateController.java
+++ backend/main/src/main/java/br/com/nathan/ecommerce/main/modules/customer/controller/DeactivateController.java
@@ -3,10 +3,14 @@ package br.com.nathan.ecommerce.main.modules.customer.controller;
 import br.com.nathan.ecommerce.main.core.annotation.BaseController;
 import br.com.nathan.ecommerce.main.core.exceptions.EntityNotFoundException;
 import br.com.nathan.ecommerce.main.core.infra.http.HttpHelper;
+import br.com.nathan.ecommerce.main.core.mapper.Mapper;
 import br.com.nathan.ecommerce.main.core.validator.TypeUtils;
+import br.com.nathan.ecommerce.main.modules.customer.controller.presentationModel.CustomerPM;
+import br.com.nathan.ecommerce.main.modules.customer.domain.Customer;
 import br.com.nathan.ecommerce.main.modules.customer.useCases.PatchActiveUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
@@ -17,12 +21,13 @@ import java.util.Optional;
 public class DeactivateController {
 
     private final PatchActiveUseCase useCase;
+    private final Mapper<Customer, CustomerPM> mapper;
 
-    @PutMapping("/{id}/deactivate")
+    @PatchMapping("/{id}/deactivate")
     public ResponseEntity<?> handle(@PathVariable Long id) {
         try {
             var data = useCase.execute(id, false);
-            return HttpHelper.ok(Optional.of(data));
+            return HttpHelper.ok(Optional.of(mapper.map(data)));
         } catch (Exception exception) {
             if(TypeUtils.is(exception, EntityNotFoundException.class)) {
                 return HttpHelper.badRequest(exception);

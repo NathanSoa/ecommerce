32b652cd33df1e345bc955aff37747610743ae4d
Nathan Soares <nathansoares809@gmail.com>
2023-11-23 16:09:49 -0300

    refactor: delete unused interfaces

--- backend/main/src/main/java/br/com/nathan/ecommerce/main/core/infra/messaging/MessagingConsumer.java
+++
@@ -1,6 +0,0 @@
-package br.com.nathan.ecommerce.main.core.infra.messaging;
-
-public interface MessagingConsumer<T> {
-
-    void consume(T message);
-}
--- backend/main/src/main/java/br/com/nathan/ecommerce/main/core/infra/messaging/MessagingProducer.java
+++
@@ -1,6 +0,0 @@
-package br.com.nathan.ecommerce.main.core.infra.messaging;
-
-public interface MessagingProducer<T> {
-
-    void send(T message);
-}

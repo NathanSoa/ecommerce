ad61e2bd38768967ec826570c566306182467305
Nathan Soares <nathansoares809@gmail.com>
2023-11-27 17:37:46 -0300

    feat: implement card flag validation

---
+++ backend/main/src/main/java/br/com/nathan/ecommerce/main/modules/customer/business/ValidateCardFlag.java
@@ -0,0 +1,28 @@
+package br.com.nathan.ecommerce.main.modules.customer.business;
+
+import br.com.nathan.ecommerce.main.core.exceptions.BusinessException;
+import br.com.nathan.ecommerce.main.core.interfaces.Strategy;
+import br.com.nathan.ecommerce.main.modules.cardFlag.useCases.FindAllUseCase;
+import br.com.nathan.ecommerce.main.modules.customer.domain.Card;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+public class ValidateCardFlag implements Strategy<List<Card>> {
+
+    private final FindAllUseCase findAllFlags;
+
+    @Override
+    public void process(List<Card> object) {
+        final var flags = findAllFlags.execute()
+                .stream()
+                .map(flag -> flag.getName().toUpperCase()).toList();
+
+        object.forEach(card -> {
+            if(!(flags.contains(card.getName().toUpperCase()))) {
+                throw new BusinessException("card.flag.invalid");
+            }
+        });
+    }
+}
